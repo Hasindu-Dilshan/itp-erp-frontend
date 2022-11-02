17564f7b906a4ed5f52465dfa140de3688caa428
tharukerajapaksha123 <darksparrow156@gmail.com>
2022-11-02 18:15:23 +0530

    ui done

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import './App.css';
-import Dashboard from './components/dashboard/Dashboard';
+import Signin from './components/auth/Signin';
+//import Dashboard from './components/dashboard/Dashboard';
 //import Signin from './components/auth/Signin';
 
 function App() {
   return (
     <div className="App">
-      <Dashboard/>
+      <Signin/>
     </div>
   );
 }
