febd3f54e98721b562ac5aee6a134ce1c1a4a94f
tharukerajapaksha123 <darksparrow156@gmail.com>
2022-11-03 10:53:15 +0530

    customer ui done

--- src/components/customers/Customer.tsx
+++ src/components/customers/Customer.tsx
@@ -1,73 +1,134 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 
 import { PlusCircleOutlined } from '@ant-design/icons';
 import CustomRow from '../common/Row';
 import WrapperContainer from '../common/WrapperContainer'
-import { Button, Tooltip } from 'antd'
+import { Button, Space, Tooltip } from 'antd'
 import { Typography } from 'antd';
 import Table, { ColumnsType } from 'antd/lib/table';
 import { CustomeModel } from '../../models/customer_model';
 import AddCustomerModal from './AddCustomerModal';
+import CustomerService from '../../services/customer_service';
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
 const { Title } = Typography;
 
-const columns: ColumnsType<CustomeModel> = [
-  {
-    title: "ID",
-    dataIndex: "id",
-    key: "id"
-  },
-  {
-    title: "Name",
-    dataIndex: "name",
-    key: "name"
-  },
-  {
-    title: "NIC",
-    dataIndex: "nic",
-    key: "nic"
-  },
-  {
-    title: "Mobile Numer",
-    dataIndex: "mobile-number",
-    key: "mobile-number"
-  },
-  {
-    title: "Email",
-    dataIndex: "email",
-    key: "email"
-  },
-  {
-    title: "Address",
-    dataIndex: "address",
-    key: "address"
-  },
 
-]
 
 
 const Customer = () => {
+  const [customers, setCustomers] = useState<CustomeModel[]>([]);
+  const [selectedCustomer, setSelectedCustomer] = useState<CustomeModel>();
+  const [isEditModalOpen, setIsEditaModalOpen] = useState<boolean>(false);
+  const [isDeleteModalOpen, setDeleteAddModalOpen] = useState<boolean>(false);
 
   const [isAddCustomerOpen, setIsAddCustomerOpen] = useState<boolean>(false);
 
-  const openAddCuastomerModal = () => {
-    setIsAddCustomerOpen(true);
+  const openAddCuastomerModal = async () => {
+    await refresher();
+    setIsAddCustomerOpen(!openAddCuastomerModal);
+
   }
 
-  const closeAddCustomerModal = () => {
+  const closeAddCustomerModal = async () => {
+    await refresher();
     setIsAddCustomerOpen(false);
   }
+
+
+  const columns: ColumnsType<CustomeModel> = [
+    {
+      title: "ID",
+      dataIndex: "_id",
+      key: "id"
+    },
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name"
+    },
+    {
+      title: "NIC",
+      dataIndex: "nic",
+      key: "nic"
+    },
+    {
+      title: "Mobile Numer",
+      dataIndex: "mobile",
+      key: "mobile-number"
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email"
+    },
+    {
+      title: "Address",
+      dataIndex: "address",
+      key: "address"
+    },
+    {
+      title: "Actions",
+      key: "status",
+      render: (_, record: CustomeModel) => {
+        return <Space size="middle">
+          <Button icon={<EditOutlined />} onClick={() => {
+            const d: CustomeModel = {
+              _id: record._id,
+              name: record.name,
+              date: record.date,
+              address: record.address,
+              companyId: record.companyId,
+              nic: record.nic,
+              email: record.email,
+              mobile: record.mobile,
+            }
+            setSelectedCustomer(d)
+            setIsEditaModalOpen(true)
+          }}></Button>
+          <Button icon={<DeleteOutlined />} onClick={() => {
+            const d: CustomeModel = {
+              _id: record._id,
+              name: record.name,
+              date: record.date,
+              address: record.address,
+              companyId: record.companyId,
+              nic: record.nic,
+              email: record.email,
+              mobile: record.mobile,
+
+            }
+            setSelectedCustomer(d);
+            setDeleteAddModalOpen(true)
+          }}></Button>
+        </Space>
+      }
+    },
+  ]
+
+  const refresher = async () => {
+    await CustomerService.getCustomers(1, 10).then(result => {
+      setCustomers([...result])
+    });
+  }
+
+  useEffect(() => {
+    CustomerService.getCustomers(1, 10).then(result => {
+      setCustomers([...result])
+    });
+  }, [])
 
   return (
     <WrapperContainer>
       <CustomRow>
         <Title level={3}>Customers</Title>
         <Tooltip title="Add Delivery Order">
-          <Button type="primary" shape="circle" icon={<PlusCircleOutlined />} onClick={() => { setIsAddCustomerOpen(true)}} />
+          <Button type="primary" shape="circle" icon={<PlusCircleOutlined />} onClick={() => { setIsAddCustomerOpen(true) }} />
         </Tooltip>
       </CustomRow>
-      <Table columns={columns} className="table" dataSource={[]} />
+      <Table columns={columns} className="table" dataSource={customers} />
       <AddCustomerModal handleOk={openAddCuastomerModal} handleCancel={closeAddCustomerModal} isOpen={isAddCustomerOpen} />
+      <AddCustomerModal handleOk={async()=>{await refresher();setIsEditaModalOpen(false);}} handleCancel={()=>{setIsEditaModalOpen(false);}} isOpen={isEditModalOpen}  customer={selectedCustomer}/>
     </WrapperContainer>
   )
 }
---
+++ src/services/customer_service.ts
@@ -0,0 +1,61 @@
+
+import http, { post, put } from "../http-common"
+import { CustomeModel } from "../models/customer_model"
+import companyId from "../config"
+const url = "http://127.0.0.1:8080";
+
+
+const getCustomers = async (offset: number, pagination: number): Promise<CustomeModel[]> => {
+   return await http.get(`customer/${companyId.companyId}/${pagination}/${offset}`).then((result) => {
+      return result.data.customers;
+   }).catch(err => {
+      console.log(`get sales orders failed ${err}`);
+      return [];
+   }
+   )
+
+}
+
+const updateCustomer = (id: number, Customer: CustomeModel) => {
+   put(url + "/update-customer/" + { id },
+      {
+         date: Customer.date,
+         name: Customer.name,
+         nic: Customer.nic,
+         mobile: Customer.mobile,
+         email: Customer.email,
+         address: Customer.address,
+
+         companyId: Customer.companyId,
+      }
+   ).then(result => result.status);
+}
+
+const createCustomer = async (Customer: CustomeModel) => {
+ 
+
+   post(url + "/customer/create-customer",
+      {
+         date: Customer.date,
+         name: Customer.name,
+         nic: Customer.nic,
+         mobile: Customer.mobile,
+         email: Customer.email,
+         address: Customer.address,
+         companyId: Customer.companyId,
+      }
+   ).then(result => {
+      console.log(result.data);
+      return result.status;
+   }).catch(err => console.log(err));
+   // console.log("order created")
+
+}
+
+const deleteCustomer = async (id: string) => {
+   await http.delete(`customer/delete-customer/${id}`).then(result => result.status);
+}
+
+const CustomerService = { getCustomers, updateCustomer, createCustomer, deleteCustomer }
+
+export default CustomerService
