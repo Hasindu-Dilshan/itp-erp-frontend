5e6958199fee1f75b8c2bf750ba8a7f3bd0a53ff
IsuruSanjaya <isurusanjayaa@gmail.com>
2022-11-05 10:47:38 +0530

    fix edit

--- src/services/stock_order_service.ts
+++ src/services/stock_order_service.ts
@@ -2,7 +2,7 @@
 import http, { post, put } from "../http-common"
 import { StockOrderModel } from "../models/stock_order_model"
 import companyId from "../config"
-const url = "http://127.0.0.1:8080";
+const url = "http://127.0.0.1:8090";
 
 
 const getOrderQty = async (offset: number, pagination: number): Promise<StockOrderModel[]> => {
