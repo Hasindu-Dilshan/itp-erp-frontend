1fb48fe64d0715129ed8299b72612a620ae05839
LENOVO <it21067860@my.sliit.lk>
2022-11-06 12:09:24 +0530

    bug remove

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Signin from './components/auth/Signin';
 import Dashboard from './components/dashboard/Dashboard';
-import Signin from './components/auth/Signin';
+
 import SignUp from './components/auth/SignUp';
 import { reactLocalStorage } from 'reactjs-localstorage';
 
@@ -31,19 +31,13 @@ function App() {
 
   return (
     <div className="App">
-<<<<<<< HEAD
+
       
-      <Signin /> 
-=======
-<<<<<<< HEAD
-      <Dashboard/>
-      {/* <SignUp /> */}
-=======
+   
       {
         loggedIn ? <Dashboard stateChanger={stateChanger} /> : userLoggedIn === "NOT_LOGGED_IN" ? <Signin stateChanger={() => { stateChanger() }} /> : userLoggedIn === "SIGNUP" ? <SignUp stateChanger={() => { stateChanger() }} /> : <Dashboard stateChanger={() => { stateChanger() }} />
       }
->>>>>>> 54a0771b8398f572a47d1b8c3ddd9812b9d4cd98
->>>>>>> ef1de16b156b209ba323303ce796d33e1b825c79
+
     </div>
   );
 }
