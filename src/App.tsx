a58bcdf6fe731c50eaa0615acec1436565f4d811
IsuruSanjaya <isurusanjayaa@gmail.com>
2022-11-06 11:44:55 +0530

    dashboard added

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 
 import './App.css';
-//import Signin from './components/auth/Signin';
+import Signin from './components/auth/Signin';
 import Dashboard from './components/dashboard/Dashboard';
 import SignUp from './components/auth/SignUp';
+import Home from './components/dashboard/home';
 
 function App() {
   return (
     <div className="App">
-      {/* <Dashboard/> */}
-      <SignUp />
+      <Dashboard/>
+      {/* <SignUp /> */}
     </div>
   );
 }
--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -17,6 +17,7 @@ import Users from "../Users/Users";
 import StockOrder from "../StockOrder/StockOrder";
 
 import { BankTwoTone,UserOutlined ,MoneyCollectFilled,LineChartOutlined,StockOutlined,TeamOutlined ,ShoppingCartOutlined,CarFilled} from '@ant-design/icons'
+import Home from "./home";
 
 const { Header, Content, Footer, Sider } = Layout;
 
@@ -55,7 +56,7 @@ const Dashboard = () => {
     const [activeIndex, setActiveIndex] = useState(0);
 
     const bodyContainer: JSX.Element[] = [
-        <DashboardBody />,
+        <Home />,
         <Customer />,
         <DeliveryOrder />,
         <SalesOrder />,
